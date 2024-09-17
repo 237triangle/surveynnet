402a316c5c49cd753db47b1de4b9b5bd85b9be74
Aaron Cohen <cohenaa@indiana.edu>
2024-09-17 09:59:15 -0400

    adding sample data, documenting

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Simple body fat example data
+#'
+#'
+#'
+#' @format ## `body_fat`
+#' A data frame with 12 rows and 9 columns:
+#' \describe{
+#'   \item{Subject}{Subject ID}
+#'   \item{group}{group}
+#'   \item{Weight_kg}{weight}
+#'   \item{Height_cm}{height}
+#'   \item{Age}{age}
+#'   \item{pct_body_fat}{percent body fat}
+#'   \item{survey_wt}{survey weight}
+#'   \item{stratum}{stratum}
+#'   \item{cluster}{cluster}
+#'
+#' }
+"body_fat"
